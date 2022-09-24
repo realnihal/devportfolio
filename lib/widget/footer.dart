493c85d909d6bf504fe451ad543a73bedeafae04
realnihal <puramnihal@gmail.com>
2022-09-25 01:56:40 +0530

    added firebase support

--- lib/widget/footer.dart
+++ lib/widget/footer.dart
@@ -20,7 +20,7 @@ class Footer extends StatelessWidget {
               "Developed in 💙 with ",
             ),
             InkWell(
-              onTap: () => openURL("https://github.com/realnihal/Devfolio"),
+              onTap: () => openURL("https://github.com/realnihal/devportfolio"),
               child: const Text(
                 "Flutter",
                 style: TextStyle(color: Colors.blue),
